606cef2bc8882d2f1e0b7e084ae09cec1fb9e199
panaki-gillot <gillotpanaki@gmail.com>
2026-06-19 14:39:55 +0200

    Add measure_runtime function to measure elapsed time of four parallel async_comprehension calls

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Measure runtime of four parallel async_comprehension calls"""
+import asyncio
+import time
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Run async_comprehension four times in parallel, return elapsed time."""
+    start = time.perf_counter()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    return time.perf_counter() - start
